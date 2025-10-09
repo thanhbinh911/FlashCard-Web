69c774e93a78e5a334f07363f093344c660c6c18
Binh Giang <chaubinhvip123@gmail.com>
2025-10-10 00:21:07 +0700

    SpringBoot template strucutre folder

---
+++ src/main/java/hust/soict/ict/flashcard_web/FlashcardWebApplication.java
@@ -0,0 +1,13 @@
+package hust.soict.ict.flashcard_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashcardWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashcardWebApplication.class, args);
+	}
+
+}
