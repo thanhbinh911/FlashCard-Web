6ac98870ce9f3f427cd2227993e5daf816bdd40b
Tsuki <hung5008@gmail.com>
2025-11-03 21:26:37 +0700

    Change name 2 file login and register

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,8 @@
 
 import { Routes, Route } from "react-router-dom"
 import FlashcardPage from "./FlashcardPage"
-import Login from "./Login"
-import Register from "./Register"
+import Login from "./LoginPage"
+import Register from "./RegisterPage"
 
 function App() {
   return (
