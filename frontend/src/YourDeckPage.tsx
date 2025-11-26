110e70901a9cb3f9a9f75b5996ec156a82fbf2ba
Tsuki <hung5008@gmail.com>
2025-11-27 00:04:22 +0700

    Add your-deck css

--- frontend/src/YourDeckPage.tsx
+++ frontend/src/YourDeckPage.tsx
@@ -1,28 +1,30 @@
 import React from 'react'
 import Deck from './Deck'
 import type { deck } from './model/deckModel'
+import './style/YourDeck.css'
 
 function YourDeckPage() {
-
-  const handleAddCard = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    // Logic to add a new card to the deck goes here
-  }
 
   return (
     <div className="your-deck-page">
       <h2>Your Decks</h2>
       {/* Logic to display user's decks goes here */}
       <div className='deck-container'>
-        <Deck deck={SAMPLE_DECK} />
+        <Deck deck={SAMPLE_DECK1} />
+        <Deck deck={SAMPLE_DECK2} />
       </div>
     </div>
   )
 }
 
-const SAMPLE_DECK: deck = {
+const SAMPLE_DECK1: deck = {
   id: 1,
   title: 'Sample Deck'
+}
+
+const SAMPLE_DECK2: deck = {
+  id: 2,
+  title: 'Another Deck'
 }
 
 export default YourDeckPage
