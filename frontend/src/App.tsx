f327062b135c092b4121107c30e1ea5da26b5335
Tsuki <hung5008@gmail.com>
2025-12-17 12:54:18 +0700

    Fix bug which you can't fetch flashcard data

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
       <Route path="/register" element={<Register />} />
       <Route path="/create-deck" element={<CreateDeckPage />} />
       <Route path="/your-deck" element={<YourDeckPage />} />
-      <Route path="/decks/:deckId" element={<DeckPage  />} />
+      <Route path="/decks/:deckId/:deckTitle" element={<DeckPage  />} />
       <Route path="/decks/:deckId/add-flashcard" element={<AddCardPage  />} />
     </Routes>
   )
