3582e93d6c2aa2de71c51d24a8eb8acfc0ef9c56
Tsuki <hung5008@gmail.com>
2026-01-02 19:47:02 +0700

    update baseline

--- frontend/src/login/LoginPage.tsx
+++ frontend/src/login/LoginPage.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import './style/LoginPage.css'; // <--- Đừng quên import CSS
+import '../style/LoginPage.css'; // <--- Đừng quên import CSS
 
 interface AuthResponse {
   token: string;
